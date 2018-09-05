34c6f019a42b14f03424930e782a005a9369b7a1
carlos martin <cjmar87@gmail.com>
2018-09-05 13:51:54 -0500

    empezando leccion 15

--- src/app/components/password/response-reset/response-reset.component.ts
+++ src/app/components/password/response-reset/response-reset.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { JarvisService } from '../../../Services/jarvis.service';
 
 @Component({
   selector: 'app-response-reset',
@@ -7,9 +9,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ResponseResetComponent implements OnInit {
 
-  constructor() { }
+  public error = [];
+
+  public form = {
+    email: null,
+    password: null,
+    password_confirmation: null,
+    resetToken: null
+  };
+
+  constructor(
+    private route: ActivatedRoute,
+    private Jarvis: JarvisService
+  ) {
+    route.queryParams.subscribe(params => {
+      this.form.resetToken = params['token']
+    });
+   }
 
   ngOnInit() {
+  }
+
+  onSubmit(){
+    this.Jarvis.changePassword(this.form).subscribe(
+      data => this.handleResponse(data),
+      error => this.handleError(error)
+    );
+  }
+
+  handleResponse(data){
+
+  }
+
+  handleError(error){
+
   }
 
 }
